3e98a06020cf45c44934a0f69439967ed15594c3
danie <danie@DESKTOP-QKJDL3I>
2022-11-23 21:05:26 +0100

    play funktion hinzugefügt

--- app/src/main/java/de/buw/se4de/App.java
+++ app/src/main/java/de/buw/se4de/App.java
@@ -7,13 +7,28 @@ import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.io.IOException;
 import java.util.concurrent.atomic.AtomicReference;
+import java.io.File;
+import javax.sound.sampled.*;
 
 public class App {
+	//Spielt eine vorhandene .wav Datei ab.
+	public static void play(String filename) {
+		try {
+			Clip clip = AudioSystem.getClip();
+			clip.open(AudioSystem.getAudioInputStream(new File(filename)));
+			clip.start();
+		}
+		catch (Exception exc) {
+			exc.printStackTrace(System.out);
+		}
+	}
 	public static void main(String[] args) throws IOException{
 		JFrame mainFrame = new JFrame("Thanidria");
 		mainFrame.setSize(900, 600);
 		mainFrame.setResizable(false);
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		JButton play = new JButton("Play");
 
 		JButton export = new JButton("Export");
 
@@ -42,7 +57,35 @@ public class App {
 			}
 		});
 
-		AtomicReference<String> NAME = new AtomicReference<>("none");
+		AtomicReference<String> nameWAV = new AtomicReference<>("none");
+		AtomicReference<String> playWAV = new AtomicReference<>("none");
+
+		play.addActionListener(e -> {
+			JFrame frame = new JFrame("Play");
+			frame.setSize(400, 300);
+			frame.setLocation(100, 150);
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+			JLabel text = new JLabel("Gebe den Namen der abzuspielenden Datei ein.", SwingConstants.CENTER);
+			text.setBounds(50,50,300,30);
+
+			JTextField textfield = new JTextField();
+			textfield.setBounds(50, 100, 300, 30);
+
+			JButton ok = new JButton("OK");
+			ok.setBounds(150, 150, 100, 30);
+
+			ok.addActionListener(f ->{
+				nameWAV.set(textfield.getText());
+				play(nameWAV+".wav");
+				frame.dispose();
+			});
+			frame.add(text);
+			frame.add(textfield);
+			frame.add(ok);
+			frame.setLayout(null);
+			frame.setVisible(true);
+		});
 
 		name.addActionListener(e -> {
 			JFrame frame = new JFrame("Name");
@@ -50,21 +93,19 @@ public class App {
 			frame.setLocation(100, 150);
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-			JLabel text = new JLabel("Gebe deiner Datei einen Namen.");
-			text.setBounds(50,50,200,30);
+			JLabel text = new JLabel("Gebe deiner Datei einen Namen.", SwingConstants.CENTER);
+			text.setBounds(50,50,300,30);
 
 			JTextField textfield = new JTextField();
-			textfield.setBounds(50, 100, 200, 30);
+			textfield.setBounds(50, 100, 300, 30);
 
 			JButton ok = new JButton("OK");
-			ok.setBounds(50, 150, 200, 30);
+			ok.setBounds(150, 150, 100, 30);
 
 			ok.addActionListener(f ->{
-					NAME.set(textfield.getText());
-					frame.dispose();
-				});
-
-
+				nameWAV.set(textfield.getText());
+				frame.dispose();
+			});
 			frame.add(text);
 			frame.add(textfield);
 			frame.add(ok);
@@ -73,10 +114,11 @@ public class App {
 		});
 
 		export.addActionListener(e -> {
-			gui.exportMusic(NAME.get(), 4.0);
+			gui.exportMusic(nameWAV.get(), 4.0);
 		});
 
 		JPanel panel = new JPanel();
+		panel.add(play, BorderLayout.NORTH);
 		panel.add(export, BorderLayout.NORTH);
 		panel.add(name, BorderLayout.NORTH);
 		mainFrame.getContentPane().add(panel, BorderLayout.NORTH);
