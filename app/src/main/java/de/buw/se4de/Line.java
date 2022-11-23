fa1fe65dbcf1f2ff3eeeccc91731645d948098e7
danie <daniel.ocks@uni-weimar.de>
2022-11-23 12:47:55 +0100

    Linie können wild umher gezogen werden, WaveHandler sorgt dafür das die Linien dann richtig ausgegeben werden

--- app/src/main/java/de/buw/se4de/Line.java
+++ app/src/main/java/de/buw/se4de/Line.java
@@ -12,11 +12,8 @@ public class Line {
         this.b = PtB;
         this.length = PtA.getDistance(PtB);
     }
-    public void paint(Graphics g) {
-        //super.paint(g);
-        Graphics2D g2 = (Graphics2D) g;
-        g2.setStroke(new BasicStroke(3));
-        g2.drawLine(a.xCoordinate, a.yCoordinate, b.xCoordinate, a.yCoordinate);
+    public void draw(Graphics g) {
+        g.drawLine(a.xCoordinate, a.yCoordinate, b.xCoordinate, b.yCoordinate);
     }
 
 }
---
+++ app/src/main/java/de/buw/se4de/WaveHandler.java
@@ -0,0 +1,42 @@
+package de.buw.se4de;
+
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+
+// das hier ist das selbe wie im Github repo
+// schreibt einfach nur die gesammelten Samples in eine .wav Datei
+public class WaveHandler {
+    public static void writeFile(String name, float[] buffer, double sampleRate) throws IOException {
+        final byte[] byteBuffer = new byte[buffer.length * 2];
+
+        int bufferIndex = 0;
+        for (int i = 0; i < byteBuffer.length; i++) {
+            final int x = (int)(buffer[bufferIndex++] * 32767.0);
+
+            byteBuffer[i++] = (byte)x;
+            byteBuffer[i] = (byte)(x >>> 8);
+        }
+
+        //File out = new File("src/main/resources/"+name+".wav");
+        File out = new File(name+".wav");
+
+        final boolean bigEndian = false;
+        final boolean signed = true;
+
+        final int bits = 16;
+        final int channels = 1;
+
+        AudioFormat format = new AudioFormat((float)sampleRate, bits, channels, signed, bigEndian);
+        ByteArrayInputStream bais = new ByteArrayInputStream(byteBuffer);
+        AudioInputStream audioInputStream = new AudioInputStream(bais, format, buffer.length);
+        AudioSystem.write(audioInputStream, AudioFileFormat.Type.WAVE, out);
+        audioInputStream.close();
+
+        System.out.println("File saved");
+    }
+}
