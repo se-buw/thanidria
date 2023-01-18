29d58fc8566692fb62878e7d651f44a2ad4853eb
asciini <nils.durner@uni-weimar.de>
2023-01-18 13:11:14 +0100

    put buttons below drawing surface
    also fixed some minor errors (variable naming, unused import statement)

--- app/src/main/java/de/buw/se4de/App.java
+++ app/src/main/java/de/buw/se4de/App.java
@@ -5,14 +5,13 @@ import javax.swing.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
-import java.io.IOException;
 import java.util.concurrent.atomic.AtomicReference;
 import java.io.File;
 import javax.sound.sampled.*;
 
 public class App {
 	//Spielt eine vorhandene .wav Datei ab.
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args){
 
 		// Creating the main window of the app and specifying its properties
 		JFrame mainFrame = new JFrame("Thanidria");
@@ -20,13 +19,12 @@ public class App {
 		mainFrame.setResizable(false);
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		JButton play = new JButton("Play");
+		// The GUI element is where the user will draw their song
+		GUI gui = new GUI();
 
+		JButton play = new JButton("Play");
 		JButton save = new JButton("Save");
 
-		//JButton name = new JButton("Name");
-
-		GUI gui = new GUI();
 
 		gui.addMouseMotionListener(new MouseMotionListener() {
 			@Override
@@ -57,11 +55,12 @@ public class App {
 			frame.setSize(400, 300);
 			frame.setLocation(100, 150);
 
+
 			JLabel text = new JLabel("Gebe den Namen der abzuspielenden Datei ein.", SwingConstants.CENTER);
 			text.setBounds(50,50,300,30);
 
-			JTextField textfield = new JTextField();
-			textfield.setBounds(50, 100, 300, 30);
+			JTextField textField = new JTextField();
+			textField.setBounds(50, 100, 300, 30);
 
 			JButton ok = new JButton("OK");
 			ok.setBounds(150, 150, 100, 30);
@@ -70,43 +69,16 @@ public class App {
 			frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 
 			ok.addActionListener(f ->{
-				nameWAV.set(textfield.getText());
+				nameWAV.set(textField.getText());
 				play("app/src/audios/"+nameWAV+".wav");
 				frame.dispose();
 			});
 			frame.add(text);
-			frame.add(textfield);
+			frame.add(textField);
 			frame.add(ok);
 			frame.setLayout(null);
 			frame.setVisible(true);
 		});
-		/*
-		name.addActionListener(e -> {
-			JFrame frame = new JFrame("Name");
-			frame.setSize(400, 300);
-			frame.setLocation(100, 150);
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-			JLabel text = new JLabel("Gebe deiner Datei einen Namen.", SwingConstants.CENTER);
-			text.setBounds(50,50,300,30);
-
-			JTextField textfield = new JTextField();
-			textfield.setBounds(50, 100, 300, 30);
-
-			JButton ok = new JButton("OK");
-			ok.setBounds(150, 150, 100, 30);
-
-			ok.addActionListener(f ->{
-				nameWAV.set(textfield.getText());
-				frame.dispose();
-			});
-			frame.add(text);
-			frame.add(textfield);
-			frame.add(ok);
-			frame.setLayout(null);
-			frame.setVisible(true);
-		});
-		*/
 
 		save.addActionListener(e -> {
 			JFrame frame = new JFrame("Save");
@@ -117,8 +89,8 @@ public class App {
 			JLabel text = new JLabel("Gebe deiner Datei einen Namen.", SwingConstants.CENTER);
 			text.setBounds(50,50,300,30);
 
-			JTextField textfield = new JTextField();
-			textfield.setBounds(50, 100, 300, 30);
+			JTextField textField = new JTextField();
+			textField.setBounds(50, 100, 300, 30);
 
 			JButton ok = new JButton("OK");
 			ok.setBounds(150, 150, 100, 30);
@@ -127,9 +99,9 @@ public class App {
 
 
 			ok.addActionListener(f ->{
-				nameWAV.set(textfield.getText());
+				nameWAV.set(textField.getText());
 
-				String name = textfield.getText();
+				String name = textField.getText();
 				String specialCharacters = "!@#$%&*()'+,-./:;<=>?[]^_`{|}";
 
 				if (name.length() == 0){
@@ -150,7 +122,7 @@ public class App {
 
 			});
 			frame.add(text);
-			frame.add(textfield);
+			frame.add(textField);
 			frame.add(ok);
 			frame.setLayout(null);
 			frame.setVisible(true);
@@ -158,10 +130,12 @@ public class App {
 
 		// hier wurde ein Panel erstellt, mit Knöpfen versehen und daraufhin mit dem main frame verbunden
 		JPanel panel = new JPanel();
+
 		panel.add(play, BorderLayout.NORTH);
 		panel.add(save, BorderLayout.NORTH);
-		mainFrame.getContentPane().add(panel, BorderLayout.NORTH);
+
 		mainFrame.getContentPane().add(gui);
+		mainFrame.getContentPane().add(panel, BorderLayout.SOUTH);
 		mainFrame.validate();
 		mainFrame.setVisible(true);
 	}
